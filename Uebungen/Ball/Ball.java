11e1aced361a031604d204837a6827e7e07a2183
Marco <kiefer.marco@me.com>
2017-12-05 21:59:59 +0100

    Klasse Ball geänder, Methode druckeInfo hinzugefuegt

--- Uebungen/Ball/Ball.java
+++ Uebungen/Ball/Ball.java
@@ -1,18 +1,18 @@
 
-/**
- * Beschreiben Sie hier die Klasse Ball.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+/** 
+ * Klasse Ball
+ * @author (Marco Kiefer) 
+ * @version (05.12.2017)
  */
 public class Ball{
-
     private double maxFuellmenge;
     private double aktuelleFuellmenge;
-
+    private double druck;
+    
     public Ball(double maxFuellmenge){
         this.maxFuellmenge = maxFuellmenge;
         this.aktuelleFuellmenge = 0.0;
+        this.druck = 0.0;
     }
 
     public double getMaxFuellmenge(){
@@ -28,10 +28,11 @@ public class Ball{
         if(aufgepumt >= maxFuellmenge){
             System.out.println("Zu viel druck, der Ball hält maximal "+maxFuellmenge+" Bar aus");
             aktuelleFuellmenge = maxFuellmenge;
-            } else {
+        } else {
             aktuelleFuellmenge = aufgepumt;
         }
     }
+
     public void luftRausLassen(double menge){
         double luftraus = aktuelleFuellmenge - menge;
         if(luftraus < 0){
@@ -40,10 +41,18 @@ public class Ball{
             aktuelleFuellmenge = luftraus;
         }
     }
+
     public double getLuftdruckInProzent(){
         double druck = maxFuellmenge / 100 * aktuelleFuellmenge;
         druck = druck * 100;
+        this.druck = druck;
         System.out.println(druck +" %");
         return druck;
+    }
+
+    public void druckeInfo(){
+        System.out.println("Aktuelle Füllmenge: "+ aktuelleFuellmenge +", Maximale Füllmenge: "+ maxFuellmenge);
+        System.out.println("Luftdruck in Prozent: "+ druck);
+        System.out.println("----------------------------------------------------------------");
     }
 }
