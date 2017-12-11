d07b9d160bb554c6ca903d654816f24ca9160cea
Marco Kiefer, Server <i2019kiefer@gbs-technikum.de, Server>
2017-12-11 09:26:44 +0100

    Klasse Ball geändert - Methoden verbessert

--- Uebungen/Ball/Ball.java
+++ Uebungen/Ball/Ball.java
@@ -7,12 +7,11 @@
 public class Ball{
     private double maxFuellmenge;
     private double aktuelleFuellmenge;
-    private double druck;
-    
+
     public Ball(double maxFuellmenge){
         this.maxFuellmenge = maxFuellmenge;
         this.aktuelleFuellmenge = 0.0;
-        this.druck = 0.0;
+
     }
 
     public double getMaxFuellmenge(){
@@ -24,35 +23,42 @@ public class Ball{
     }
 
     public void aufpumpen(double fuellmenge){
+        if(fuellmenge < 0){
+            System.out.println("Werte unter Null nicht erlaubt");
+            return;
+        } 
         double aufgepumt = aktuelleFuellmenge + fuellmenge;
         if(aufgepumt >= maxFuellmenge){
             System.out.println("Zu viel druck, der Ball hält maximal "+maxFuellmenge+" Bar aus");
             aktuelleFuellmenge = maxFuellmenge;
-        } else {
+        } else  {
             aktuelleFuellmenge = aufgepumt;
         }
     }
 
     public void luftRausLassen(double menge){
+        if(menge > 0){
+            System.out.println("Werte unter Null nicht erlaubt");
+            return;
+        } 
         double luftraus = aktuelleFuellmenge - menge;
         if(luftraus < 0){
             aktuelleFuellmenge = 0.0;
-        } else {
+        } else  {
             aktuelleFuellmenge = luftraus;
         }
     }
 
     public double getLuftdruckInProzent(){
-        double druck = maxFuellmenge / 100 * aktuelleFuellmenge;
-        druck = druck * 100;
-        this.druck = druck;
-        System.out.println(druck +" %");
-        return druck;
+        double prozent = maxFuellmenge / 100 * aktuelleFuellmenge;
+        prozent = prozent * 100;
+        //System.out.println(prozent +" %");
+        return prozent;
     }
 
     public void druckeInfo(){
         System.out.println("Aktuelle Füllmenge: "+ aktuelleFuellmenge +", Maximale Füllmenge: "+ maxFuellmenge);
-        System.out.println("Luftdruck in Prozent: "+ druck);
+        System.out.println("Luftdruck in Prozent: "+ (maxFuellmenge / 100 * aktuelleFuellmenge)*100);
         System.out.println("----------------------------------------------------------------");
     }
 }
