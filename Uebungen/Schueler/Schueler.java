eae999023a0dcea673ca50b13547cb601669faf9
Marco Kiefer, Server <i2019kiefer@gbs-technikum.de, Server>
2017-12-12 13:14:54 +0100

    Klasse Schueler + Schulklasse erstellt

---
+++ Uebungen/Schueler/Schueler.java
@@ -0,0 +1,54 @@
+
+/**
+ * Write a description of class Schueler here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Schueler
+{
+
+    private String name;
+    private int alter;
+    private Schueler mitSchueler; 
+
+    public Schueler(){
+        this.name = "-Kein Alter";
+        this.alter = -1;
+        this.mitSchueler = null;
+    }
+
+    public Schueler(String name, int alter){
+        this.name = name;
+        this.alter = alter;
+        this.mitSchueler = null;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAlter(){
+        return alter;
+    }
+
+    public Schueler getMitSchueler(){
+        return mitSchueler;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAlter(int alter){
+        this.alter = alter;
+    }
+
+    public void setMitSchueler(Schueler verweisSchueler){
+        this.mitSchueler = verweisSchueler;
+    }
+    
+    public void druckeInfo(){
+        System.out.println("Name: "+ name + ", Alter: " + alter + ", Name des Mitschülers: "+ mitSchueler.getName());
+    }
+}
---
+++ Uebungen/Schueler/Schulklasse.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Schulklasse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Schulklasse
+{
+    private String bezeichnung;
+    private Schueler schueler0;
+    private Schueler schueler1;
+    private Schueler schueler2;
+        
+    public Schulklasse(){
+        this.bezeichnung = "Keine Bezeichnung angegeben";
+        this.schueler0 = null;
+        this.schueler1 = null;
+        this.schueler2 = null;
+    }
+    public Schulklasse(String bezeichnung){
+        this.bezeichnung = bezeichnung;
+        
+            }
+    public Schulklasse(Sting bezeichnung, Schueler schueler0, Schueler schueler1, Schueler schueler2,){
+        this.bezeichnung = bezeichnung;
+        this.schueler0 = schueler0;
+        this.schueler
+    }
+
+    
+   }
